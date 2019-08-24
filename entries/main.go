8bf72d2af03e35312f0c37b10b23d6d6e558a4a2
Jim Pick <jim@backblend.com>
2019-08-24 15:30:22 -0700

    Export log data (wrong format)

--- entries/main.go
+++ entries/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -44,6 +45,7 @@ func main() {
 	}
 
 	http.HandleFunc("/entries", handleEntries)
+	http.HandleFunc("/vis-data/", handleVisData)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8011"
@@ -59,6 +61,7 @@ var pageTemplate = template.Must(template.New("").Parse(`
     <tr>
         <td>
             {{$i}} {{.DateTime}}
+            <a href="/vis-data/{{.TraceID}}">Vis Data</a>
             <a href="https://console.cloud.google.com/logs/viewer?authuser=0&project=dht-test-249818&minLogLevel=0&expandAll=false&customFacets=&limitCustomFacetWidth=true&interval=JUMP_TO_TIME&resource=cloud_run_revision%2Fservice_name%2Fdht-test-2&dateRangeStart={{.StartTime}}&timestamp={{.StartTime}}&advancedFilter=resource.type%3D%22cloud_run_revision%22%0Aresource.labels.service_name%3D%22dht-test-2%22%0AlogName%3D%22projects%2Fdht-test-249818%2Flogs%2Frun.googleapis.com%252Fstderr%22%20OR%20logName%3D%22projects%2Fdht-test-249818%2Flogs%2Frun.googleapis.com%252Fstdout%22%0Alabels.instanceId%3D%22{{.Entry.Labels.instanceId}}%22&dateRangeEnd=2019-08-24T21:18:43.000Z&scrollTimestamp={{.StartTime}}" target="_blank">Instance</a>
             <a href="https://console.cloud.google.com/logs/viewer?authuser=0&project=dht-test-249818&minLogLevel=0&expandAll=false&timestamp=2019-08-24T07:15:45.499000000Z&customFacets=&limitCustomFacetWidth=true&interval=NO_LIMIT&resource=project&scrollTimestamp=2019-08-23T20:46:33.361358546Z&advancedFilter=resource.type%3D%22project%22%0Alabels.trace%3D%22{{.TraceID}}%22%0Alabels.type%3D%22log%22" target="_blank">Logs</a>
             <a href="https://console.cloud.google.com/logs/viewer?authuser=0&project=dht-test-249818&minLogLevel=0&expandAll=false&timestamp=2019-08-24T07:15:45.499000000Z&customFacets=&limitCustomFacetWidth=true&interval=NO_LIMIT&resource=project&scrollTimestamp=2019-08-23T20:46:33.361358546Z&advancedFilter=resource.type%3D%22project%22%0Alabels.trace%3D%22{{.TraceID}}%22%0Alabels.type%3D%22event%22" target="_blank">Events</a>
@@ -124,5 +127,29 @@ func handleEntries(w http.ResponseWriter, r *http.Request) {
 	}
 	if _, err := buf.WriteTo(w); err != nil {
 		log.Printf("writing response: %v", err)
+	}
+}
+
+func handleVisData(w http.ResponseWriter, r *http.Request) {
+	traceID := strings.Replace(r.URL.Path, "/vis-data/", "", 1)
+	ctx := context.Background()
+	filter := fmt.Sprintf(
+		`resource.type="project" `+
+			`AND labels.trace="%s" `+
+			`AND labels.type="event"`,
+		traceID)
+	it := client.Entries(ctx, logadmin.Filter(filter))
+	for {
+		entry, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			panic("Error")
+		}
+		data, err := json.Marshal(entry.Payload)
+		if err == nil {
+			fmt.Fprintf(w, "data: %v\n\n", string(data))
+		}
 	}
 }
