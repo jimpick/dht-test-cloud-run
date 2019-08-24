14f85d8a29720f6df0a304f0f0355517b169571a
Jim Pick <jim@backblend.com>
2019-08-24 00:22:52 -0700

    Parse entries, link to Google Stackdriver Logging

--- entries/main.go
+++ entries/main.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 
 	"cloud.google.com/go/logging"
 	"cloud.google.com/go/logging/logadmin"
@@ -18,6 +19,12 @@ var (
 	client    *logadmin.Client
 	projectID = flag.String("project-id", "", "ID of the project to use")
 )
+
+type parsedEntry struct {
+	Entry    *logging.Entry
+	DateTime string
+	TraceID  string
+}
 
 func main() {
 	// This example demonstrates how to iterate through items a page at a time
@@ -43,7 +50,13 @@ func main() {
 var pageTemplate = template.Must(template.New("").Parse(`
 <table>
   {{range .Entries}}
-    <tr><td>{{.Payload}}</td></tr>
+    <tr>
+        <td>
+            {{.DateTime}} {{.TraceID}}
+            <a href="https://console.cloud.google.com/logs/viewer?authuser=0&project=dht-test-249818&minLogLevel=0&expandAll=false&timestamp=2019-08-24T07:15:45.499000000Z&customFacets=&limitCustomFacetWidth=true&interval=NO_LIMIT&resource=project&scrollTimestamp=2019-08-23T20:46:33.361358546Z&advancedFilter=resource.type%3D%22project%22%0Alabels.trace%3D%22{{.TraceID}}%22%0Alabels.type%3D%22log%22">Logs</a>
+            <a href="https://console.cloud.google.com/logs/viewer?authuser=0&project=dht-test-249818&minLogLevel=0&expandAll=false&timestamp=2019-08-24T07:15:45.499000000Z&customFacets=&limitCustomFacetWidth=true&interval=NO_LIMIT&resource=project&scrollTimestamp=2019-08-23T20:46:33.361358546Z&advancedFilter=resource.type%3D%22project%22%0Alabels.trace%3D%22{{.TraceID}}%22%0Alabels.type%3D%22event%22">Events</a>
+        </td>
+    </tr>
   {{end}}
 </table>
 {{if .Next}}
@@ -68,15 +81,30 @@ func handleEntries(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("problem getting the next page: %v", err), http.StatusInternalServerError)
 		return
 	}
+	parsedEntries := make([]parsedEntry, len(entries))
+	for i, entry := range entries {
+		var values []string
+		if str, ok := entry.Payload.(string); ok {
+			values = strings.Split(str, " ")
+		} else {
+			panic("Wrong type")
+		}
+		parsedEntries[i] = parsedEntry{
+			Entry:    entry,
+			DateTime: values[0] + " " + values[1],
+			TraceID:  values[4],
+		}
+	}
+
 	data := struct {
-		Entries []*logging.Entry
+		Entries []parsedEntry
 		Next    string
 	}{
-		entries,
+		parsedEntries,
 		nextTok,
 	}
 	for i, v := range data.Entries {
-		fmt.Println("Jim", i, v.Payload, v.Resource.Labels)
+		fmt.Println("Jim", i, v.TraceID)
 	}
 	var buf bytes.Buffer
 	if err := pageTemplate.Execute(&buf, data); err != nil {
